5873f187e9506e866c21e382ab35f692e8ffcc36
hwhaocool <yellow.tail@qq.com>
2026-02-24 16:58:43 +0800

    翻译文件改为相对路径

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -4,7 +4,7 @@ import { initReactI18next } from 'react-i18next';
 import Backend from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
 // don't want to use this?
-// have a look at the Quick start guide 
+// have a look at the Quick start guide
 // for passing in lng and translations on init
 
 i18n
@@ -23,6 +23,10 @@ i18n
     lng: 'zh-CN',
     fallbackLng: 'zh-CN',
     debug: false,
+
+    backend: {
+      loadPath: './locales/{{lng}}/{{ns}}.json',
+    },
 
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default
