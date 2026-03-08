fa3057d856379340d24a089225d0798598914a4c
hwhaocool <yellow.tail@qq.com>
2026-03-08 17:35:19 +0800

    fix github page error

--- src/document.tsx
+++ src/document.tsx
@@ -10,6 +10,7 @@ export default function Document() {
       <head>
         <meta charSet="utf-8" />
         <meta name="description" content={description} />
+        <base href="/fabritor-web/" />
         <link rel="icon" href="./favicon.ico" />
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, viewport-fit=cover" />
         <Meta />
